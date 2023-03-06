8055d6917b5bab901fb6b77deb60d839cb2ffa68
bharadwajponna <bharadwajponna01@gmail.com>
2023-03-06 14:03:20 +0530

    Added Hello api view

--- profiles_api/admin.py
+++ profiles_api/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
-
+from profiles_api.models import UserProfile
+from django.contrib.auth.admin import UserAdmin
 # Register your models here.
+
+# class AdminUser(UserAdmin):
+#     ordering = ('email', 'name')
+#     list_display = ('email', 'name')
+#     search_fields = ('email', 'name')
+
+
+admin.site.register(UserProfile)
--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
-
+from rest_framework.views import APIView
+from rest_framework.response import Response
 # Create your views here.
+
+
+
+class HelloApiView(APIView):
+    """ Test api view """
+
+    def get(self, request, format=None):
+        """ returns a list of apiview features """
+        an_apiview = [
+            'uses http methods as function(get,post,put,patch,delete)',
+            'is similar to traditional django view',
+            'give you the most control over you applicatin login',
+            'is mapped manually to url',
+        ]
+
+        return Response({'message':"hello!",
+                         'an_apiview':an_apiview})
