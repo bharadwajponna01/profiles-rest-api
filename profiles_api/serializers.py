456313ea9db695817f4728bc524f3715d1f5a61a
bharadwajponna <bharadwajponna01@gmail.com>
2023-03-06 14:49:32 +0530

    Added Post, Put, Patch, delete methods to our apiviews

---
+++ profiles_api/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+
+
+class HelloSerializer(serializers.Serializer):
+    """ serializers a name field for testing our apiview """
+    name = serializers.CharField(max_length=40)
+    
+     
